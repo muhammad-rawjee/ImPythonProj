bd6023529eb669b62a6bd0d3af0c7e438bc46604
Muhammad <throwawayali100@gmail.com>
2025-06-23 03:31:14 +0530

    Edits

--- ProjectOne/pig_latin.py
+++ ProjectOne/pig_latin.py
@@ -1,5 +1,16 @@
+CONSONANTS = set(list("bcdfghjklmnpqrstvwxyz"))
+VOWELS = set(list("aeiou"))
 # Get Input
 sentenceToConvert = input("What did say?\n")
 # Convert input into list
 sentenceToWordList = sentenceToConvert.split()
-print(sentenceToWordList)
+# Convert each word into pig latin
+def pigLatinConverter(wordList):
+    res = []
+    print(wordList)
+    for word in wordList:
+        if word[0].lower() in CONSONANTS:
+            # Deal with consonant clusters later
+            new_word = 
+        pass
+        
